c602b6594cc6994febba6daa4c494675c16c89b3
Vitty97 <alvarezgonzal@gmail.com>
2018-10-17 13:03:33 +0200

    Ahora hay cesped y el sol se mueve imitando el amanecer

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square grass;
 
     /**
      * Constructor for objects of class Picture
@@ -47,13 +48,27 @@ public class Picture
         roof.moveHorizontal(20);
         roof.moveVertical(-60);
         roof.makeVisible();
+        
+        grass = new Square();
+        grass.changeColor("green");
+        grass.moveHorizontal(-330);
+        grass.moveVertical(140);
+        grass.changeSize(900);
+        grass.makeVisible();
+    
 
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(140);
         sun.changeSize(80);
         sun.makeVisible();
+        sun.slowMoveVertical(-140);
+        
+        
+        
+
+        
     }
 
     /**
